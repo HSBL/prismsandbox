6d85bc3516906b75379425e038cbce00c68db3bc
unknown <amilhasbala@gmail.com>
2021-09-22 13:58:34 +0700

    create module

---
+++ ModuleProject/ModuleName.cs
@@ -0,0 +1,20 @@
+﻿using ModuleProject.ViewModels;
+using ModuleProject.Views;
+using Prism.Ioc;
+using Prism.Modularity;
+
+namespace ModuleProject
+{
+    public class ModuleName : IModule
+    {
+        public void OnInitialized(IContainerProvider containerProvider)
+        {
+
+        }
+
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            containerRegistry.RegisterForNavigation<ViewA, ViewAViewModel>();
+        }
+    }
+}
